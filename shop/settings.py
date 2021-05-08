58fa9b34978fb440b9cd57bf23db2cc122f98712
Kdheeru12 <60852451+Kdheeru12@users.noreply.github.com>
2021-05-08 21:32:06 +0530

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -141,7 +141,8 @@ STATIC_ROOT = '/static/'
 STATICFILES_DIRS = (
  os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+'
 
 
 
